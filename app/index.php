570381ed3ab07f53879381a57238696d83993979
Cybergeist <satejgandre@hotmail.com>
2024-09-02 22:20:01 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,11 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Geist</title>
+        <style>
+            body {
+                font-family:sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
 <?php
-// Enter your code here, enjoy!
-$array = array("1" => "PHP code tester Sandbox Online",
-    "emoji" => "😀 😃 😄 😁 😆", 5 , 5 => 89009,
-    "Random number" => rand(100,999),
-    "PHP Version" => phpversion()
-);
-
-foreach( $array as $key => $value ){
-    echo $key."\t=>\t".$value."\n";
-}
+$response = "<p style='color:purple;'>PHP is working.</p>"
+?>
+<hr/> 
+</body>
+</html>
