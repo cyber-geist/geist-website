8a1c475fc0df831670ece325b483988f96f96b02
Cybergeist <satejgandre@hotmail.com>
2024-09-26 13:34:03 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -9,10 +9,16 @@
         </style>
     </head>
     <body>
-        <h1>Hello, world!</h1>
-<?php
-echo "<p style='color:purple;'>PHP is working.</p>";
-?>
+        <h1>IHS CyberPatriot</h1>
+        <p>Team members:</p>
+        <ul>
+            <li>Satej Gandre - <em>Windows</em></li>
+            <li>Rohit Shivarajkumar - <em>Linux</em></li>
+            <li>Krithik Ravichandren - <em>Cisco Packet Tracer</em></li>
+            <li>Rushil Ramakumar - <em>Cisco Packet Tracer</em></li>
+            <li>Rohan Chilukuri - <em>Windows</em></li>
+            <li>El Elyon Akeya - <em>Linux</em></li>
+        </ul>
 <hr/> 
 </body>
 </html>
